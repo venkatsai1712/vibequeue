d9460a270de729d9fcf2f64bdb8f82b6b80b6e76
Venkat Sai <illavenkatsai1712@gmail.com>
2025-09-13 18:34:32 +0530

    Logo Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
-  
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
